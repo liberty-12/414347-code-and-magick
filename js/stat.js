8def86f44ddb4ca98682bb0a84ab2c8eec7909f8
Екатерина <liberty_12@mail.ru>
2018-09-02 21:01:11 +0300

    Form clouds

--- js/stat.js
+++ js/stat.js
@@ -13,7 +13,18 @@ var BAR_HEIGHT = 150;
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo((x + CLOUD_WIDTH), y);
+  ctx.lineTo((x + CLOUD_WIDTH) - GAP, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo((x + CLOUD_WIDTH), (y + CLOUD_HEIGHT));
+  ctx.lineTo(x + CLOUD_WIDTH / 2, (y + CLOUD_HEIGHT) - GAP);
+  ctx.lineTo(x, (y + CLOUD_HEIGHT));
+  ctx.lineTo(x + GAP, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x, y);
+  ctx.closePath();
+  ctx.fill();
 };
 
 var getMaxElement = function(arr) {
@@ -33,7 +44,7 @@ window.renderStatistics = function(ctx, players, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
-  ctx.font = "16px Arial";
+  ctx.font = "16px PT Mono";
 
   var congratulation = ['Ура, вы победили!', 'Список результатов:'];
 
