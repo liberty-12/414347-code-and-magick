d9f7cac716f9e988c378c4cf9156da93fcb171c2
Екатерина <liberty_12@mail.ru>
2018-09-03 16:07:04 +0300

    Add renderBarChart, edit getMaxElement

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,8 @@ var TEXT_HEIGHT = 16;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
+var congratulation = ['Ура, вы победили!', 'Список результатов:'];
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
 
@@ -27,48 +29,42 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fill();
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var renderBarChart = function (ctx, name, time) {
+  var maxTime = getMaxElement(time);
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+  for (var i = 0; i < name.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(time[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT - (BAR_HEIGHT * Math.round(time[i])) / maxTime);
+
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (name[i] !== 'Вы') {
+      ctx.fillStyle = 'rgb(0, 0, 255, ' + Math.round((Math.random() * 1) * 10) / 10 + ')';
     }
-  }
 
-  return maxElement;
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, BAR_WIDTH, -((BAR_HEIGHT * Math.round(time[i])) / maxTime));
+
+    ctx.fillStyle = '#000';
+    ctx.fillText(name[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
+  }
+};
+
+var getMaxElement = function (arr) {
+  return Math.max.apply(null, arr);
 };
 
 var showMessage = function (ctx, message) {
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+
   for (var i = 0; i < message.length; i++) {
     ctx.fillText(message[i], CLOUD_X + BAR_GAP, CLOUD_Y + GAP + FONT_GAP + GAP * 2 * i);
   }
 };
 
-window.renderStatistics = function (ctx, players, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-
-  var congratulation = ['Ура, вы победили!', 'Список результатов:'];
   showMessage(ctx, congratulation);
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP * 2 - TEXT_HEIGHT - (BAR_HEIGHT * Math.round(times[i])) / maxTime);
-
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    if (players.names !== 'Вы') {
-      ctx.fillStyle = 'rgb(0, 0, 255, ' + Math.round((Math.random() * 1) * 10) / 10 + ')';
-    }
-
-    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP - TEXT_HEIGHT, BAR_WIDTH, -((BAR_HEIGHT * Math.round(times[i])) / maxTime));
-
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
-  }
+  renderBarChart(ctx, names, times);
 };
